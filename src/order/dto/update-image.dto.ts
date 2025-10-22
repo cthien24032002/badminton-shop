25e7ca66b179be9388002944407a4b0d5283c4d1
cthien24032002 <cthien24032002@gmail.com>
2025-10-22 22:10:21 +0700

    fix transformer for update image order

--- src/order/dto/update-image.dto.ts
+++ src/order/dto/update-image.dto.ts
@@ -1,9 +1,11 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsEnum, IsNotEmpty, IsNumber } from "class-validator";
 import { OrderStatus } from "src/common/enums";
+import { Type } from 'class-transformer';
 
 export class UpdateImageOrderDto {
     @IsNotEmpty({message: 'Id không được để trống'})
+    @Type(()=>Number)
     @IsNumber({},{message:'Id phải là 1 số'})
     @ApiProperty({
         example: 1,
